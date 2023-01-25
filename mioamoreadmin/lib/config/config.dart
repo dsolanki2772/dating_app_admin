b94e70c7bf458aa57a38b0d03b4f57569079fd37
devShakib <devshakib015@gmail.com>
2023-01-25 09:11:19 +0600

    Preparing to release!

--- mioamoreadmin/lib/config/config.dart
+++ mioamoreadmin/lib/config/config.dart
@@ -1,9 +1,5 @@
-import 'package:flutter/material.dart';
-
 class AppConstants {
   AppConstants._();
 
   static const String appName = 'mio amore admin';
-
-  static const Color primaryColor = Color.fromARGB(255, 242, 7, 85);
 }
